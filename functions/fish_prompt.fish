e12bb14f8416d5e56d1be79a5e2dab03b10522b9
USAMI Kosuke <usami.kosuke@gmail.com>
2023-07-23 22:31:01 +0900

    Use variables in function

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -6,12 +6,6 @@ set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showupstream 'informative'
 set __fish_git_prompt_describe_style 'default'
 
-set --query GRAYSTATUS_COLOR_PROMPT; or set GRAYSTATUS_COLOR_PROMPT brblack
-set --query GRAYSTATUS_COLOR_STATUS; or set GRAYSTATUS_COLOR_STATUS red
-set --query GRAYSTATUS_SHOW_STATUS; or set GRAYSTATUS_SHOW_STATUS 1
-set --query GRAYSTATUS_SHOW_PROMPT; or set GRAYSTATUS_SHOW_PROMPT 1
-set --query GRAYSTATUS_SHOW_RIGHT_PROMPT; or set GRAYSTATUS_SHOW_RIGHT_PROMPT 1
-
 function prompt_rbenv
     if not type -q 'rbenv'
         return
@@ -35,6 +29,10 @@ end
 
 function fish_prompt
     set -l last_status $status
+    set --query GRAYSTATUS_SHOW_STATUS; or set GRAYSTATUS_SHOW_STATUS 1
+    set --query GRAYSTATUS_SHOW_PROMPT; or set GRAYSTATUS_SHOW_PROMPT 1
+    set --query GRAYSTATUS_COLOR_STATUS; or set GRAYSTATUS_COLOR_STATUS red
+    set --query GRAYSTATUS_COLOR_PROMPT; or set GRAYSTATUS_COLOR_PROMPT brblack
     printf "\033[K"
     if test $GRAYSTATUS_SHOW_STATUS -ne 0
         if test $last_status -ne 0
@@ -54,6 +52,8 @@ function fish_prompt
 end
 
 function fish_right_prompt
+    set --query GRAYSTATUS_SHOW_RIGHT_PROMPT; or set GRAYSTATUS_SHOW_RIGHT_PROMPT 1
+    set --query GRAYSTATUS_COLOR_PROMPT; or set GRAYSTATUS_COLOR_PROMPT brblack
     if test $GRAYSTATUS_SHOW_RIGHT_PROMPT -ne 0
         set_color $GRAYSTATUS_COLOR_PROMPT
         echo -n (prompt_rbenv)
